72e6f32412526b0d0b14f360d3bd90af56ccf92f
handecavus <handeecavus@gmail.com>
2023-06-23 19:43:11 +0300

    teacher login oluşturuldu

---
+++ src/test/java/FeatureFiles/TeacherLogin/TeacherLogin.feature
@@ -0,0 +1,6 @@
+Feature: Teacher Login Functionality
+
+  Scenario: Login with walid username and password
+    Given Navigate to Campus
+    And Enter walid username, password and click to login button
+    Then User should login successfully
---
+++ src/test/java/StepDefinitions/TeacherLogin/TeacherLoginSteps.java
@@ -0,0 +1,19 @@
+package StepDefinitions.TeacherLogin;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class TeacherLoginSteps {
+    @Given("Navigate to Campus")
+    public void navigateToCampus() {
+    }
+
+    @And("Enter walid username, password and click to login button")
+    public void enterWalidUsernamePasswordAndClickToLoginButton() {
+    }
+
+    @Then("User should login successfully")
+    public void userShouldLoginSuccessfully() {
+    }
+}
