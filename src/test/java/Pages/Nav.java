cbddfbc4a4d1e8974b95ced587c953262a8a0d82
handecavus <handeecavus@gmail.com>
2023-06-24 13:29:57 +0300

    Nav updated

--- src/test/java/Pages/Nav.java
+++ src/test/java/Pages/Nav.java
@@ -13,5 +13,36 @@ public class Nav extends Parent{
     @FindBy(xpath = "//span[text()='Techno Study Intern School']")
     public WebElement text;
 
+    @FindBy(xpath = "//ms-layout-menu-button[@page='COURSES']")
+    public WebElement courses;
+
+    @FindBy(xpath = "//ms-layout-menu-button[@page='CALENDAR']")
+    public WebElement calendar;
+
+    @FindBy(xpath = "//ms-layout-menu-button[@page='ATTENDANCE']")
+    public WebElement attendance;
+
+    @FindBy(xpath = "//ms-layout-menu-button[@page='ASSIGNMENT']")
+    public WebElement assignment;
+
+    @FindBy(xpath = "//ms-layout-menu-button[@page='GRADING']")
+    public WebElement grading;
+
+    @FindBy(xpath = "(//button[@aria-haspopup='menu'])[1]")
+    public WebElement burgerMenu;
+
+
+    public WebElement getWebElement(String element){
+        switch (element){
+            case "text":       return text;
+            case "courses":    return courses;
+            case "calendar":   return calendar;
+            case "attendance": return attendance;
+            case "assignment": return assignment;
+            case "grading":    return grading;
+            case "burgerMenu": return burgerMenu;
+        }
+        return null;
+    }
 
 }
