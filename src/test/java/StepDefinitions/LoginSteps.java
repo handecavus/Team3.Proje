89edb361bced03706655296efb0b62d43b81a7a1
handecavus <handeecavus@gmail.com>
2023-06-24 20:19:00 +0300

    LoginSteps updated

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -43,39 +43,9 @@ public class LoginSteps {
                 dc.clickFunction(dc.loginButton);
                 break;
 
-            case "student1":
+            case "student":
                 dc.sendKeysFunction(dc.username, data.get(2).get(0));
-                dc.sendKeysFunction(dc.password, data.get(2).get(1));
-                dc.clickFunction(dc.loginButton);
-                break;
-
-            case "student3":
-                dc.sendKeysFunction(dc.username, data.get(3).get(0));
-                dc.sendKeysFunction(dc.password, data.get(3).get(1));
-                dc.clickFunction(dc.loginButton);
-                break;
-
-            case "student4":
-                dc.sendKeysFunction(dc.username, data.get(4).get(0));
-                dc.sendKeysFunction(dc.password, data.get(4).get(1).substring(0,5));
-                dc.clickFunction(dc.loginButton);
-                break;
-
-            case "student5":
-                dc.sendKeysFunction(dc.username, data.get(5).get(0));
-                dc.sendKeysFunction(dc.password, data.get(5).get(1).substring(0,5));
-                dc.clickFunction(dc.loginButton);
-                break;
-
-            case "student8":
-                dc.sendKeysFunction(dc.username, data.get(6).get(0));
-                dc.sendKeysFunction(dc.password, data.get(6).get(1).substring(0,5));
-                dc.clickFunction(dc.loginButton);
-                break;
-
-            case "student10":
-                dc.sendKeysFunction(dc.username, data.get(7).get(0));
-                dc.sendKeysFunction(dc.password, data.get(7).get(1).substring(0,5));
+                dc.sendKeysFunction(dc.password, data.get(2).get(1).substring(0,5));
                 dc.clickFunction(dc.loginButton);
                 break;
         }
