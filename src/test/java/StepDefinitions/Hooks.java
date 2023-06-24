393fd6d9b3ba739ae336fcb0c2857f930abef6ac
handecavus <handeecavus@gmail.com>
2023-06-24 13:34:48 +0300

    Hooks updated

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -1,5 +1,6 @@
 package StepDefinitions;
 
+import Utilities.ExcelUtility;
 import Utilities.GWD;
 import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
@@ -11,6 +12,8 @@ public class Hooks {
     @After
     public void after(Scenario scenario){
         System.out.println("Senaryo bitti");
+
+        ExcelUtility.writeExcel("src/test/java/ApachePOI/ScenarioStatus.xlsx", scenario, GWD.threadBrowserGet());
 
         if (scenario.isFailed()){
             TakesScreenshot ts = (TakesScreenshot) GWD.getDriver();
